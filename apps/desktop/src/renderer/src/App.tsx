be8db2a6d4d9499e352257c40b0aea91e3f4a3eb
zolinthecow <colinzhao777@gmail.com>
2024-07-27 13:50:07 -0700

    add shadcn

--- apps/desktop/src/renderer/src/App.tsx
+++ apps/desktop/src/renderer/src/App.tsx
@@ -1,11 +1,14 @@
+import { Button } from '@/components/ui/button';
+
 function App(): JSX.Element {
   // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
     <>
       <h1 className="text-4xl font-bold underline">Hello world!</h1>
+      <Button variant="outline">hi</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
